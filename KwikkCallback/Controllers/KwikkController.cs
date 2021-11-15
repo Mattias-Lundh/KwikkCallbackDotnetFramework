c943f02a5d08daeda59b278086f958cdf65713ad
mattias lundh <mattiasanderslundh@gmail.com>
2021-11-15 17:16:13 +0100

    callback stored on disk example

--- KwikkCallback/Controllers/KwikkController.cs
+++ KwikkCallback/Controllers/KwikkController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.IO;
+using System.Web.Http;
+
+using Newtonsoft.Json;
 
 namespace WebApplication2.Controllers
 {
@@ -7,9 +11,24 @@ namespace WebApplication2.Controllers
         [HttpPost]
         public string Callback(CallbackDto callbackDto)
         {
-            // ToDo: interesting things with data contained in [callbackDto]
+            var fileLocation = @"c:/kwikk/callback.json";
 
-            return "I have received some data";
+            Directory.CreateDirectory("c:/kwikk/");
+            var fileStream = File.Create(fileLocation);
+
+            using (var writer = new StreamWriter(fileStream))
+            {
+                writer.Write(JsonConvert.SerializeObject(callbackDto));
+            }
+
+            return $"the callbackDto has been written to file, path: {fileLocation}";
+        }
+
+        [HttpGet]
+        public string Test()
+        {
+            return "This is working fine";
+
         }
     }
 }
